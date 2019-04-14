5bfbc499f70e901fb559cb6520db53bdeaf9b41b
lahlim <lassi_mustonen@hotmail.com>
2019-04-14 09:13:08 +0300

    anecdotes valmis

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -1,28 +1,67 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-
 // nappi
 const Button = ({ handleClick, text }) => {
-    return (
-      <button onClick={handleClick}>
-        {text}
-      </button>
-    )
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
+// votes komponentti
+const Votes = ({ votes, selected }) => (
+  <p>has {votes[selected]} votes</p>
+)
+
+// Eniten ääniä saanut komponentti, etsitään ja tulostetaan
+const MostVotes = ({ anecdotes, votes }) => {
+  let max = 0;
+  let index = 0;
+  for (let i in votes) {
+    if (max < votes[i]) {
+      max = votes[i];
+      index = i;
+    }
   }
 
+  if(max === 0) return <h2>Vote for anecdote</h2>;
+
+  return (
+    <>
+      <h2>Anecdote with most votes</h2>
+      <p>{anecdotes[index]}</p>
+      <p>has {max} votes</p>
+    </>
+  )
+}
+
 const App = (props) => {
-  const [selected, setSelected] = useState(0)
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(props.votes);
 
   const handleClick = () => {
-    setSelected(Math.floor(Math.random() * anecdotes.length));
+    let rnd = Math.floor(Math.random() * anecdotes.length);
+    while (rnd === selected) {
+      rnd = Math.floor(Math.random() * anecdotes.length);
+    }
+    setSelected(rnd);
+  }
+
+  const handleVote = () => {
+    let copy = [...votes];
+    copy[selected] += 1;
+    setVotes(copy);
   }
 
   return (
     <div>
       {props.anecdotes[selected]}
-      <br/>
-      <Button handleClick={handleClick} text="next anecdote"/>
+      <Votes votes={votes} selected={selected} />
+      <Button handleClick={handleVote} text="vote" />
+      <Button handleClick={handleClick} text="next anecdote" />
+      <MostVotes anecdotes={props.anecdotes} votes={votes} />
     </div>
   )
 }
@@ -35,8 +74,9 @@ const anecdotes = [
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
+const votes = new Array(anecdotes.length + 1).join('0').split('').map(parseFloat);
 
 ReactDOM.render(
-  <App anecdotes={anecdotes} />,
+  <App anecdotes={anecdotes} votes={votes} />,
   document.getElementById('root')
 )
