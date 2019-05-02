dc16f84a6ebbc243a8ddd5cd89e03660aa95f892
lahlim <lassi_mustonen@hotmail.com>
2019-05-02 13:00:26 +0300

    maiden tiedot valmis

--- osa2/maiden_tiedot/src/App.js
+++ osa2/maiden_tiedot/src/App.js
@@ -1,41 +1,105 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const Weather = props => {
+  const [weather, setWeather] = useState();
+  useEffect(() => {
+    axios
+      .get(
+        `http://api.apixu.com/v1/current.json?key=9eab1e54b06c43b5bc285507190205&q=${
+          props.query
+        }`
+      )
+      .then(response => {
+        console.log(response.data);
+
+        setWeather(response.data);
+        weatherNow();
+      });
+  }, []);
+
+  const weatherNow = () => {
+    if (weather === undefined) return <div />;
+    return (
+      <div>
+        <h2>Weather in {props.query}</h2>
+        <p>
+          <b>Temperature: </b>
+          {weather.current.temp_c} celcius
+        </p>
+        <img src={weather.current.condition.icon} alt='weather image' />
+        <p>
+          <b>Wind: </b>
+          {weather.current.wind_kph} kph direction {weather.current.wind_dir}
+        </p>
+      </div>
+    );
+  };
+  return <>{weatherNow()}</>;
+};
+
 const App = () => {
-  const [countries, setCountries] = useState();
-  const [show, setShow] = useState();
+  const [data, setData] = useState([]);
+  const [show, setShow] = useState([]);
 
   useEffect(() => {
     axios.get('https://restcountries.eu/rest/v2/all').then(response => {
-      setCountries(response.data);
+      setData(response.data);
     });
   }, []);
 
+  const rows = () => {
+    if (show.length > 10) return <p>Specify filter</p>;
+    if (show.length === 1) {
+      return (
+        <>
+          <h1>{show[0].name}</h1>
+          <p>capital {show[0].capital}</p>
+          <p>population {show[0].population}</p>
+          <h2>Languages</h2>
+          <ul>
+            {show[0].languages.map((lang, index) => (
+              <li key={index}>{lang.name}</li>
+            ))}
+          </ul>
+          <Weather query={show[0].capital} />
+
+          <img key={show[0].flag} src={show[0].flag} alt='flag' />
+        </>
+      );
+    }
+
+    return show.map(country => (
+      <div key={country.numericCode}>
+        <p>{country.name}</p>
+        <button value={country.name} onClick={handleClick}>
+          show
+        </button>
+      </div>
+    ));
+  };
+
+  // handles show click
+  const handleClick = e => {
+    console.log(e.target.value);
+    handleFilter(e);
+  };
+
   const handleFilter = e => {
-    const found = countries.filter(
-      country =>
-        country.name
-          .toLocaleLowerCase()
-          .indexOf(e.target.value.toLocaleLowerCase()) > -1
+    const found = data.filter(country =>
+      country.name
+        .toLocaleLowerCase()
+        .startsWith(e.target.value.toLocaleLowerCase())
     );
     setShow(found);
   };
-
   return (
     <>
       <h1>Country info</h1>
       Find countries <input onChange={handleFilter} />
-      <Countries countriess={countries} />
+      {rows()}
     </>
   );
-};
-
-const Country = ({ country }) => <p>{country.name}</p>;
-
-const Countries = ({ countries }) => {
-  console.log(countries);
-  // fix this
-  return <div />;
 };
 
 export default App;
