62879a63d26ac123dc8575cf27dc234293986de9
Lassi <lassi_mustonen@hotmail.com>
2019-06-03 13:54:22 +0300

    Blogilista step 2 valmis

---
+++ osa4/app.js
@@ -0,0 +1,24 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const blogsRouter = require("./controllers/blogs");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+console.log("connecting to MongoDB");
+
+mongoose
+  .connect(config.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch(error => {
+    console.log("error connection to MongoDB:", error.message);
+  });
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/api/blogs", blogsRouter);
+
+module.exports = app;
