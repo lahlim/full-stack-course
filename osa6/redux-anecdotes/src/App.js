8a28e7e395dd2260c7d4118993ecd2218ad614cb
Lassi <lassi_mustonen@hotmail.com>
2019-06-17 17:49:56 +0300

    Git korjaus osa6 a

---
+++ osa6/redux-anecdotes/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AnecdoteList from './components/AnecdoteList';
+import AnecdoteForm from './components/AnecdoteForm';
+
+const App = props => {
+  return (
+    <div>
+      <AnecdoteList store={props.store} />
+      <AnecdoteForm store={props.store} />
+    </div>
+  );
+};
+
+export default App;
---
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { voteAnecdote } from '../reducers/anecdoteReducer';
+
+const AnecdoteList = ({ store }) => {
+  const anecdotes = store.getState();
+  anecdotes.sort((a, b) => b.votes - a.votes);
+
+  const vote = id => {
+    console.log('vote', id);
+    store.dispatch(voteAnecdote(id));
+  };
+  return (
+    <div className='Listing'>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AnecdoteList;
